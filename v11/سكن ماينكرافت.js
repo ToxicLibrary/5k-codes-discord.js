8c97a650d6b7bc5f54f7b12a5829573f677237ef
Gaber Elsayed <47481084+muradmohamed@users.noreply.github.com>
2021-09-19 09:42:04 +0200

    Add files via upload

---
+++ v11/سكن ماينكرافت.js
@@ -0,0 +1,13 @@
+client.on('message', message => {
+  const aa = message.content.split(" ").slice(1).join(" ");
+  if(message.content.startsWith(prefix + "skin")){
+    if(!aa) return message.reply(`:x:  -  **${prefix}skin <name>**`);
+    var ss = new Discord.RichEmbed()
+    .setTitle(`${aa}'s Skin!`)
+    .setURL(`https://minotar.net/armor/body/${aa}/100.png`)
+    .setThumbnail(`https://minotar.net/avatar/${aa}`)
+    .setImage(`https://minotar.net/armor/body/${aa}/100.png`)
+    .setFooter(`Requested By : ${message.author.tag}`, message.author.avatarURL)
+    message.channel.send(ss);
+  }
+});
